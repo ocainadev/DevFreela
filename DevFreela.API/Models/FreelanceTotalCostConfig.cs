28523f024c04ab6e09a8f141462072e30b803522
CainaDev <scaina82@gmail.com>
2024-11-19 21:27:07 -0300

    refatorando para arquitetura limpa

--- DevFreela.API/Models/FreelanceTotalCostConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DevFreela.API.Models;
-
-public class FreelanceTotalCostConfig
-{
-    public decimal Maximum { get; set; }
-    public decimal Minimum { get; set; }
-    
-}
--- DevFreela.Application/Models/ViewModels/ProjectViewModel.cs
+++ DevFreela.Application/Models/ViewModels/ProjectViewModel.cs
@@ -1,4 +1,4 @@
-﻿using DevFreela.API.Entities;
+﻿using DevFreela.Core.Entities;
 
 namespace DevFreela.API.Models;
 
